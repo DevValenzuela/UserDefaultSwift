cf4e75a4910720055ca9bd3add87b6d3f3c16816
DevValenzuela <49131215+DevValenzuela@users.noreply.github.com>
2024-01-21 02:58:04 -0500

    Update Users Default SWIFT

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
-
+    @IBAction func saveButton(_ sender: UIButton) {
+        if textLabel.text == "" {
+            let alert = UIAlertController(title: "Alerta", message: "Escribe Algo", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(ok)
+            present(alert, animated: true, completion: nil)
+        }else{
+            UserDefaults.standard.set(textLabel.text, forKey: "valor")
+            textLabel.text = ""
+            textLabel.resignFirstResponder()
+        }
+       
+    }
+    
+    @IBAction func ShowAction(_ sender: UIButton) {
+        let result  =  UserDefaults.standard.object(forKey: "valor")
+        textLabel.text = result as? String
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
